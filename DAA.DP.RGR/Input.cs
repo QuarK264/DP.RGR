59e47d3c892e0665898aa0fcf3b73c4c5ce9ef8c
QuarK264 <alexey.danilov@hotmail.com>
2014-12-18 22:55:14 +0300

    2 курс, Шаблонное программирование, РГР

---
+++ DAA.DP.RGR/Input.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAA.DP.RGR
+{
+    class Input
+    {
+        internal static int Int32(string msg)
+        {
+            Console.Write(msg);
+            ConsoleKeyInfo KeyInfo;
+            List<char> CorrectDigits = new List<char> {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-'};
+            while (true)
+            {
+                KeyInfo = Console.ReadKey(true);
+
+            }
+        }
+    }
+}
---
+++ DAA.DP.RGR/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DAA.DP.RGR
+{
+    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+            int value = Input.Int32("Введите целое число:");
+        }
+    }
+}
